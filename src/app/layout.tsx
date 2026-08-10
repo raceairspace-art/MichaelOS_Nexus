480a2d5e4341b7efa2b698cecdb2503ccfb15046
raceairspace-art <raceairspace@gmail.com>
2026-08-10 14:21:54 -0700

    Load Oliver chart evidence styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./oliver.css";
+import "./oliver-chart.css";
 import "../components/digital-oliver.css";
 
 const geist = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
